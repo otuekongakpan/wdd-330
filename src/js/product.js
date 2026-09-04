a9af75ecb36ac14823d514399da93fb09a5973a2
franklin anyaogu <fanyaogu1@byupathway.edu>
2026-09-04 10:12:13 +0100

    resolved merge conflicts and formatted code

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,15 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
-import ProductData from './ProductData.mjs';
+import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cart = getLocalStorage('so-cart') || [];
+  let cart = getLocalStorage("so-cart") || [];
 
   cart.push(product);
 
-  setLocalStorage('so-cart', cart);
+  setLocalStorage("so-cart", cart);
 }
 
 // add to cart button event handler
@@ -23,5 +23,5 @@ async function addToCartHandler(e) {
 // add listener to Add to Cart button
 
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
