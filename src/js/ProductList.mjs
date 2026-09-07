9ce4ca7a07df97ff7112fe57b5fbe721165ee27e
kabandaronald70-create <kabandaronal70@gmail.com>
2026-09-07 04:55:41 +0300

    added meta descriptions for performance

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -5,7 +5,7 @@ function productCardTemplate(product) {
   return `
     <li class="product-card">
       <a href="product_pages/?product=${product.Id}">
-        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <img src="${product.Image}" alt="Image of ${product.Name}" width="250" height="200" loading="lazy" decoding="async" />
         <h2 class="card__brand">${product.Brand.Name}</h2>
         <h3 class="card__name">${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
