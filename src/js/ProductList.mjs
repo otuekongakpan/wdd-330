df78069fc5f068d53d407ac12348b4901031238c
kabandaronald70-create <kabandaronal70@gmail.com>
2026-09-07 03:23:20 +0300

    changes in the sleepoutside project for week 2 individual activity

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,39 @@
+// src/js/ProductList.mjs
+import { renderListWithTemplate } from './utils.mjs';
+
+function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+      <a href="product_pages/?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <h2 class="card__brand">${product.Brand.Name}</h2>
+        <h3 class="card__name">${product.Name}</h3>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+  `;
+}
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData(this.category);
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    // Use the utility; clear = true ensures the list is replaced each time
+    renderListWithTemplate(
+      productCardTemplate,
+      this.listElement,
+      list,
+      'afterbegin',
+      true // ← clear the element before inserting new content
+    );
+  }
+}
