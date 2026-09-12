3bae08a0d88d0ae1a0ea37a2d5a995742e9e20ea
franklin anyaogu <fanyaogu1@byupathway.edu>
2026-09-12 01:40:36 +0100

    Finished individual activity W02

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,12 @@ import ProductData from './ProductData.mjs';
 
 import ProductDetails from './ProductDetails.mjs';
 
-const productDetails = new ProductDetails(
-  getParam('product'),
-  new ProductData('tents'),
-);
+
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
+
+// console.log(`Testing getparam - product ID is ${productId}`);
+// console.log(`testing findproductbyId result ${dataSource.findProductById(productId)}`);
+
+const productDetails = new ProductDetails(productId, dataSource);
 productDetails.init();
