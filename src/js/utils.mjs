93f0951581efd5d9709b7c27479abc052d6ba857
tambabarquoi2000-cyrpto <tambabarquoi2000@gmail.com>
2026-09-12 08:09:59 -1200

    created a branh to work on the alert task

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,10 @@ export function getParam(param){
   const product = urlParams.get('product')
   return product;
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = 'afterbegin', clear = 'false')
+{
+  if(clear) {
+    parentElement = ''
+  }
+}
