0ca3cca0475da14d6bdbb0c82c3d867e169eb5c1
Otuekong Akpan <gmaniacs85@gmail.com>
2026-09-14 10:32:21 +0100

    minor fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import Alert from "./Alert";
+import Alert from "./Alert.js";
 
 const dataSource = new ProductData('tents');
 const listElement = document.querySelector('.product-list');
