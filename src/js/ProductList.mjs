498d986e50c5e55e05bf997b962dc0e5e6f7f868
Sobomabo Boma Sydney <davidsydney.sb@gmail.com>
2026-09-15 02:13:07 +0100

    Add discount indicator to product listing

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -18,30 +18,43 @@ export default class ProductList {
 
     this.renderList(list);
   }
-  getProductPage(id) {
-  const pages = {
-    "880RR": "marmot-ajax-3.html",
-    "985RF": "northface-talus-4.html",
-    "985PR": "northface-alpine-3.html",
-    "344YJ": "cedar-ridge-rimrock-2.html",
-  };
 
-  return pages[id];
-}
+  getProductPage(id) {
+    const pages = {
+      "880RR": "marmot-ajax-3.html",
+      "985RF": "northface-talus-4.html",
+      "985PR": "northface-alpine-3.html",
+      "344YJ": "cedar-ridge-rimrock-2.html",
+    };
+
+    return pages[id];
+  }
 
   productCardTemplate(product) {
     const imagePath = product.Image.replace("../", "/");
 
+    let discount = "";
+
+    if (product.FinalPrice < product.SuggestedRetailPrice) {
+      const discountAmount =
+        ((product.SuggestedRetailPrice - product.FinalPrice) /
+          product.SuggestedRetailPrice) *
+        100;
+
+      discount = `<span class="discount">${Math.round(discountAmount)}% OFF</span>`;
+    }
+
     return `
       <li class="product-card">
-<a href="product_pages/${this.getProductPage(product.Id)}">
+        <a href="product_pages/${this.getProductPage(product.Id)}">
           <img
             src="${imagePath}"
             alt="${product.Name}"
           />
+          ${discount}
           <h3 class="card__brand">${product.Brand.Name}</h3>
           <h2 class="card__name">${product.Name}</h2>
-          <p class="product-card__price">$${product.ListPrice}</p>
+          <p class="product-card__price">$${product.FinalPrice}</p>
         </a>
       </li>
     `;
