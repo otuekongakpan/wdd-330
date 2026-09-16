fa6170beb07773af9c69de0db134a61c1112d392
tambabarquoi2000-cyrpto <tambabarquoi2000@gmail.com>
2026-09-16 05:39:50 -1200

    tb-commit

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./productDetails.mjs";
@@ -8,16 +7,3 @@ const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
-=======
-import { getParam } from './utils.mjs';
-
-import ProductData from './ProductData.mjs';
-
-import ProductDetails from './ProductDetails.mjs';
-
-const productDetails = new ProductDetails(
-  getParam('product'),
-  new ProductData('tents'),
-);
-productDetails.init();
->>>>>>> 6c09c649c2989d44f379a4a6a4dfba3fb691e584
