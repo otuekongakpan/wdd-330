36e6efefb034e0b7ee9a8adabd3b6b3ecbd2de06
bulyabatracy <tracybulyaba@gmail.com>
2026-09-19 14:51:59 +0300

    Add product list sorting

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -16,14 +16,80 @@ export default class ProductList {
         this.category = category;
         this.dataSource = dataSource;
         this.listElement = listElement;
+        this.products = [];
     }
 
     async init() {
-        const list = await this.dataSource.getData();
-        this.renderList(list);
+        this.products = await this.dataSource.getData();
+        this.createSortControls();
+        this.renderList(this.products);
+    }
+
+    createSortControls() {
+        const sortContainer = document.createElement('div');
+        sortContainer.classList.add('product-sort');
+
+        sortContainer.innerHTML = `
+            <label for="sort-products">Sort products by:</label>
+            <select id="sort-products">
+                <option value="name-asc">Name (A-Z)</option>
+                <option value="name-desc">Name (Z-A)</option>
+                <option value="price-asc">Price (Low to High)</option>
+                <option value="price-desc">Price (High to Low)</option>
+            </select>
+        `;
+
+        this.listElement.parentElement.insertBefore(
+            sortContainer,
+            this.listElement
+        );
+
+        const sortSelect = sortContainer.querySelector('#sort-products');
+
+        sortSelect.addEventListener('change', (event) => {
+            this.sortProducts(event.target.value);
+        });
+    }
+
+    sortProducts(sortOption) {
+        const sortedProducts = [...this.products];
+
+        switch (sortOption) {
+            case 'name-asc':
+                sortedProducts.sort((a, b) =>
+                    a.Name.localeCompare(b.Name)
+                );
+                break;
+
+            case 'name-desc':
+                sortedProducts.sort((a, b) =>
+                    b.Name.localeCompare(a.Name)
+                );
+                break;
+
+            case 'price-asc':
+                sortedProducts.sort(
+                    (a, b) => Number(a.FinalPrice) - Number(b.FinalPrice)
+                );
+                break;
+
+            case 'price-desc':
+                sortedProducts.sort(
+                    (a, b) => Number(b.FinalPrice) - Number(a.FinalPrice)
+                );
+                break;
+        }
+
+        this.renderList(sortedProducts);
     }
 
     renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.listElement, list, 'afterbegin', true);
+        renderListWithTemplate(
+            productCardTemplate,
+            this.listElement,
+            list,
+            'afterbegin',
+            true
+        );
     }
 }
