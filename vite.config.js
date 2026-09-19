e0ae376bd4adbca48aebc4e8c82d530d042eaa5b
bulyabatracy <tracybulyaba@gmail.com>
2026-09-19 21:28:21 +0300

    Fix vite config build failure

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,18 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src/',
-  publicDir: resolve(__dirname, 'src/public'),
+  root: 'src',
+  publicDir: 'public',
   build: {
     outDir: '../dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        cart: resolve(__dirname, 'src/cart/index.html'),
-        checkout: resolve(__dirname, 'src/checkout/index.html'),
-        product: resolve(__dirname, 'src/product_pages/index.html'),
-        product_listing: resolve(__dirname, 'src/product_listing/index.html'),
+        main: resolve('src/index.html'),
+        cart: resolve('src/cart/index.html'),
+        checkout: resolve('src/checkout/index.html'),
+        product: resolve('src/product_pages/index.html'),
+        product_listing: resolve('src/product_listing/index.html'),
       },
     },
   },
-  server: { port: 5174 }
 });
